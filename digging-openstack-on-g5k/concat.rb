1691e8c4b18bc4ac88009e9b8f6ca087057d8acf
msimonin <matthieu.simonin@inria.fr>
2016-02-04 14:45:02 +0100

    keep toulouse in a separated file.
    M

--- digging-openstack-on-g5k/concat.rb
+++ digging-openstack-on-g5k/concat.rb
@@ -1,14 +1,16 @@
 require 'json'
 
 # merge with toulouse jobs
-file = File.read("openstack.json")
+file = File.read("openstack_toulouse.json")
 data = JSON.parse(file)
-toulouse = data.select{|d| d["site"] == "toulouse"}
 
-file2 = File.read("openstack2.json")
-data2 = JSON.parse(file)
+# this file mustn't contain any toulouse deployment
+file2 = File.read("openstack.json")
+data2 = JSON.parse(file2)
 
-data2.concat(toulouse)
+puts data2.size
+data2.concat(data)
+puts data2.size
 File.open("openstack_new.json", "w") do |f|
   f.write(data2.to_json)
 end
