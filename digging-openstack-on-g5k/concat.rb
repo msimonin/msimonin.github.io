9dc49515a1f68ef5c78521a5b5718966470bc886
msimonin <matthieu.simonin@inria.fr>
2016-02-11 09:53:23 +0100

    update.
    M

--- digging-openstack-on-g5k/concat.rb
+++ digging-openstack-on-g5k/concat.rb
@@ -5,13 +5,13 @@ file = File.read("openstack_toulouse.json")
 data = JSON.parse(file)
 
 # this file mustn't contain any toulouse deployment
-file2 = File.read("openstack.json")
+file2 = File.read("openstack2.json")
 data2 = JSON.parse(file2)
 
 puts data2.size
 data2.concat(data)
 puts data2.size
-File.open("openstack_new.json", "w") do |f|
+File.open("openstack.json", "w") do |f|
   f.write(data2.to_json)
 end
 
